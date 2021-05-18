650fe025024295c9597091395ae23a49895de2cf
Mark Ogilvie <mogilvie@users.noreply.github.com>
2021-05-18 13:10:52 +0100

    Pass event dispatcher to encryptor

--- Encryptors/EncryptorFactory.php
+++ Encryptors/EncryptorFactory.php
@@ -5,10 +5,10 @@
  * Date: 22/10/17
  * Time: 22:43
  */
-
 namespace SpecShaper\EncryptBundle\Encryptors;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class EncryptorFactory
 {
@@ -17,10 +17,13 @@ class EncryptorFactory
     ];
 
     private $logger;
+    
+    private $dispatcher;
 
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, EventDispatcherInterface $dispatcher)
     {
         $this->logger = $logger;
+        $this->dispatcher = $dispatcher;
     }
 
     /**
@@ -30,16 +33,9 @@ class EncryptorFactory
      */
     public function createService($encryptor, $encryptKey)
     {
-
-        // Log an error if there is no value set for the encrypt_key.
-        if($encryptKey === null){
-            $this->logger->error('The bundle specshaper\encrypt-bundle requires a parameter.yml value for "encrypt_key". 
-            Use cli command "php bin/console encrypt:genkey" to create a key.');
-        }
-
         switch($encryptor){
             default:
-                $encryptor = new OpenSslEncryptor($encryptKey);
+                $encryptor = new OpenSslEncryptor($this->dispatcher, $encryptKey);
         }
 
         return $encryptor;
