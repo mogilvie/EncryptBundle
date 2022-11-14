ee09fd6cb7afe0dbd1b355c0f5df44c476294ef2
Mark Ogilvie <m.ogilvie@parolla.ie>
2022-11-14 11:45:36 +0000

    Use AsCommand instead of overriding static defaultName.

--- Command/GenKeyCommand.php
+++ Command/GenKeyCommand.php
@@ -2,6 +2,7 @@
 
 namespace SpecShaper\EncryptBundle\Command;
 
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
