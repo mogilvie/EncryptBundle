a6d90395954be26dd7b3d60abe6eef099a4c4b3a
Lpsd <40902730+Lpsd@users.noreply.github.com>
2024-07-20 23:26:44 +0100

    Update DoctrineEncryptSubscriberInterface.php

--- src/Subscribers/DoctrineEncryptSubscriberInterface.php
+++ src/Subscribers/DoctrineEncryptSubscriberInterface.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Event\PostUpdateEventArgs;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\Event\PostFlushEventArgs;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use SpecShaper\EncryptBundle\Annotations\Encrypted;
 use SpecShaper\EncryptBundle\Encryptors\EncryptorInterface;
@@ -22,6 +23,7 @@ interface DoctrineEncryptSubscriberInterface
         LoggerInterface $logger,
         Reader $annReader,
         EncryptorInterface $encryptor,
+        EntityManagerInterface $em,
         array $annotationArray,
         bool $isDisabled
     );
