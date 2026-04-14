50743ef7591648c84bcceef984f7f1cbd116b23b
Tom Siatka <47247055+tsiatka@users.noreply.github.com>
2026-04-14 09:50:26 +0200

    Fix import statement for Symfony Extension

--- src/DependencyInjection/SpecShaperEncryptExtension.php
+++ src/DependencyInjection/SpecShaperEncryptExtension.php
@@ -8,8 +8,8 @@ use SpecShaper\EncryptBundle\EventListener\EncryptEventListener;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\Extension
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
